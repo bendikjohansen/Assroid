dec307e2fc4202d5fd14a75421ec83f51c1cbe5e
bendikjohansen <bendikj@gmail.com>
2021-07-11 13:13:57 +0200

    Add strafing

--- Assroid/Player/PlayerMovement.gd
+++ Assroid/Player/PlayerMovement.gd
@@ -2,23 +2,31 @@ extends KinematicBody2D
 
 onready var initial_position = position
 
-const MAX_SPEED = 200
+const STRAFE_SPEED = 10
+const MAX_SPEED = 300
 const ACCELERATION = 5
 const FRICTION = 1
 var velocity = Vector2.ZERO
 
-const MAX_ROTATION_SPEED = 200
-const ROTATION_ACCELERATION = 5
-const ROTATION_FRICTION = 1
-var rotation_velocity = 0
-
 func _physics_process(delta):
+	var has_thrust = false
 	if Input.is_action_pressed("thrust"):
+		has_thrust = true
 		var max_velocity = Vector2.UP.rotated(rotation + PI / 2) * MAX_SPEED
 		velocity = velocity.move_toward(max_velocity, ACCELERATION)
-	else:
+	
+	if Input.is_action_pressed("strafe_left"):
+		has_thrust = true
+		velocity = velocity + Vector2.LEFT * STRAFE_SPEED
+		
+	if Input.is_action_pressed("strafe_right"):
+		has_thrust = true
+		velocity = velocity + Vector2.RIGHT * STRAFE_SPEED
+		
+	if not has_thrust:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 	
+	velocity = velocity.clamped(MAX_SPEED)
 	velocity = move_and_slide(velocity)
 
 func _input(event):
--- Assroid/Player/PlayerTrace.gd
+++ Assroid/Player/PlayerTrace.gd
@@ -2,7 +2,7 @@ extends CPUParticles2D
 
 
 func _input(event):
-	if event.is_action_pressed("ui_up"):
+	if event.is_action_pressed("thrust"):
 		emitting = true
-	elif event.is_action_released("ui_up"):
+	elif event.is_action_released("thrust"):
 		emitting = false

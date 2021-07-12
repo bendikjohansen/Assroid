f30ddf3b9fbe930f3d0d76b119e3e31de56bde2d
bendikjohansen <bendikj@gmail.com>
2021-07-12 21:16:38 +0200

    Add fixed ufo shooting interval

--- Assroid/Canon/UfoCanon.gd
+++ Assroid/Canon/UfoCanon.gd
@@ -1,11 +1,19 @@
 extends "res://Assroid/Canon/Canon.gd"
 
-onready var ufo = get_parent()
-
 const RADIUS = 25
+
+onready var ufo = get_parent()
+onready var shoot_interval = $Timer 
+var aim_direction = Vector2.ZERO
 
 func _on_UfoSight_player_sighted(player_position: Vector2, distance_to_player: float):
 	global_position = ufo.global_position.move_toward(player_position, RADIUS)
-	
-	if randf() < 0.01:
-		shoot(player_position - global_position, Color(0, 1, 0.5))
+	aim_direction = player_position - global_position
+	if shoot_interval.is_stopped():
+		shoot_interval.start()
+
+func _on_Timer_timeout():
+	shoot(aim_direction, Color(0, 1, 0.5))
+
+func _on_UfoSight_player_escaped():
+	shoot_interval.stop()

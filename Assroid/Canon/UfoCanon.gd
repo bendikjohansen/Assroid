96703d0aaa3c19e58a3ee895054643d561e7be2b
bendikjohansen <bendikj@gmail.com>
2021-07-12 21:07:04 +0200

    Fix ufo cannon position

--- Assroid/Canon/UfoCanon.gd
+++ Assroid/Canon/UfoCanon.gd
@@ -1,5 +1,11 @@
 extends "res://Assroid/Canon/Canon.gd"
 
-func _on_UfoSight_player_sighted(player_position, distance_to_player):
+onready var ufo = get_parent()
+
+const RADIUS = 25
+
+func _on_UfoSight_player_sighted(player_position: Vector2, distance_to_player: float):
+	global_position = ufo.global_position.move_toward(player_position, RADIUS)
+	
 	if randf() < 0.01:
 		shoot(player_position - global_position, Color(0, 1, 0.5))
